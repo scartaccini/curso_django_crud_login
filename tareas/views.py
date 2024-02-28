dc5233fb9fc24d01e13d0e8e1560599f4d8c6332
scartaccini <pablo.scartaccini@gmail.com>
2024-02-28 15:03:27 -0300

    se agregan tareas

--- tareas/views.py
+++ tareas/views.py
@@ -4,10 +4,12 @@ from .models import Task
 
 @login_required
 def tasks(request):
+    #busca por usuario y si datecompleted es nulo
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {"tasks": tasks})
 
 @login_required
 def tasks_completed(request):
+    #busca por usuario y si datecompleted no es nulo
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks_completed.html', {"tasks": tasks})
