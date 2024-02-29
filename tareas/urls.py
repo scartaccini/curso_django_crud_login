b917700a7199e32a7782c871097829968a78c901
scartaccini <pablo.scartaccini@gmail.com>
2024-02-29 12:32:05 -0300

    se agrea crear tarea

--- tareas/urls.py
+++ tareas/urls.py
@@ -4,5 +4,6 @@ from tareas import views
 urlpatterns = [
 path('tasks/', views.tasks, name='tasks'),
 path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
+path('create_task/', views.create_task, name='create_task'),
 ]
 
--- tareas/views.py
+++ tareas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
+from .forms import TaskForm
 
 @login_required
 def tasks(request):
@@ -13,3 +14,17 @@ def tasks_completed(request):
     #busca por usuario y si datecompleted no es nulo
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'tasks.html', {"tasks": tasks})
+
+@login_required
+def create_task(request):
+    if request.method == "GET":
+        return render(request, 'create_task.html', {"form": TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
