6f1d3084afb4bb9eadd7a3bd6d16ea92ef452b4f
scartaccini <pablo.scartaccini@gmail.com>
2024-02-29 15:04:42 -0300

    modificacion en tareas

--- tareas/views.py
+++ tareas/views.py
@@ -28,3 +28,22 @@ def create_task(request):
             return redirect('tasks')
         except ValueError:
             return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
+        
+
+"""
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
+        
+"""
