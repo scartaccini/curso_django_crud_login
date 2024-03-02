b3787545748cf347ec35b414651c71f618511bd2
scartaccini <pablo.scartacci@gmail.com>
2024-03-02 13:41:20 -0300

    bd limpia

--- tareas/urls.py
+++ tareas/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
 path('tasks/', views.tasks, name='tasks'),
 path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
 path('create_task/', views.create_task, name='create_task'),
-#path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
+path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
+path('taks/complete/<int:task_id>/', views.complete_task, name='complete_task'),
+path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
 
--- tareas/views.py
+++ tareas/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
+from django.utils import timezone
 
 @login_required
 def tasks(request):
@@ -29,21 +30,46 @@ def create_task(request):
         except ValueError:
             return render(request, 'create_task.html', {"form": TaskForm, "error": "Error creating task."})
         
+#mejor opcion usar get_object_or_404       
+'''def task_detail(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        return render(request, 'task_detail.html',{'form':task})
+    except:
+        return render(request,'task_detail.html',{'form':'NO existe'})'''       
 
-"""
+
 @login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
-        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        #task = get_object_or_404(Task, pk=task_id)
+        task = get_object_or_404(Task, pk=task_id, user=request.user) #busca tarea por id y por usuario
         form = TaskForm(instance=task)
-        return render(request, 'task_detail.html', {'task': task, 'form': form})
+        return render(request, 'task_detail.html', {'task':task, 'form': form})
     else:
         try:
+            #task = get_object_or_404(Task, pk=task_id)
             task = get_object_or_404(Task, pk=task_id, user=request.user)
             form = TaskForm(request.POST, instance=task)
             form.save()
             return redirect('tasks')
         except ValueError:
-            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
+            return render(request, 'task_detail.html', {'task':task, 'form': form, 'error': 'Error updating task.'})
         
-"""
+        
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+        
+
